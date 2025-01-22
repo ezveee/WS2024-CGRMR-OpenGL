2378d368583c96c8665cb0e1468a36888f2b9f1e
LilboX3 <lilligruber02@gmail.com>
2025-01-22 22:00:17 +0100

    FORTSCHRITTE ABER ZU DUNKEL

--- assets/shaders/fragmentShader.fs
+++ assets/shaders/fragmentShader.fs
@@ -2,9 +2,22 @@
 out vec4 FragColor;
 
 in vec2 TexCoord;
+in vec3 Normal;
+in vec3 FragPos;
 
 uniform sampler2D texture1;
+uniform vec3 lightPos;
 
 void main() {
-    FragColor = texture(texture1, TexCoord);
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    //diff = 1.0;
+
+    vec3 diffuse = diff * vec3(1.0, 1.0, 1.0);
+
+    vec4 textureColor = texture(texture1, TexCoord);
+    vec4 result = vec4(diffuse, 1.0) * textureColor;
+    FragColor = result;
+    //FragColor = textureColor;
 }
--- assets/shaders/vertexShader.vs
+++ assets/shaders/vertexShader.vs
@@ -1,13 +1,18 @@
 #version 330 core
 layout(location = 0) in vec3 aPos;
 layout(location = 1) in vec2 aTexCoord;
+layout (location = 2) in vec3 aNormal;
 
 uniform mat4 projection;
 uniform mat4 model;
 
 out vec2 TexCoord;
+out vec3 Normal;
+out vec3 FragPos;
 
 void main() {
     gl_Position = projection * model * vec4(aPos, 1.0);
+    FragPos = vec3(model * vec4(aPos, 1.0));
     TexCoord = aTexCoord;
+    Normal = aNormal;
 }
